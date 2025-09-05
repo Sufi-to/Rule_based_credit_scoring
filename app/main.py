37bc2207cc978a25374b69d916358904cb6c5e43
Tomas Teclehaimanot <80114437+Sufi-to@users.noreply.github.com>
2025-09-05 13:19:10 +0000

    cors config

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title="Credit Scoring API", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
-    #allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
